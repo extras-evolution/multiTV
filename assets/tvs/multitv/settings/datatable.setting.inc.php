290d90ae841d23d8980b6414ff083b8c3dee283f
zatomant <zatomant@gmail.com>
2022-10-09 19:08:38 +0300

    Update datatable.setting.inc.php
    add mtvCrop, Cropper.js

--- assets/tvs/multitv/settings/datatable.setting.inc.php
+++ assets/tvs/multitv/settings/datatable.setting.inc.php
@@ -4,13 +4,17 @@ $settings['css'] = array(
 	'css/dataTables.css',
 	'css/easytabs.css',
 	'css/contextMenu.css',
-  'css/colorpicker.css'
+  'css/colorpicker.css',
+    '../../lib/cropper/cropper.min.css'
 );
 $settings['scripts'] = array(
-	'js/jquery-colorbox-1.4.33.min.js',
+	'js/jquery-colorbox-1.6.4.min.js',
 	'js/jquery-dataTables-1.9.4.min.js',
 	'js/jquery-easytabs-3.2.0b.min.js',
 	'js/jquery-dataTables.rowReordering-1.1.0.js',
 	'js/jquery-contextMenu-1.7.js',
-  'js/colorpicker.js'
+    'js/colorpicker.js',
+    '../../lib/cropper/cropper.min.js',
+    '../../lib/cropper/jquery-cropper.min.js',
+    'js/mtvCrop.js',
 );
